d4c557a7023decfdeccc5bfae20ceeb39d59c898
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-07-08 15:50:16 -0300

    Inicial: agregar proyecto Django y configurar .gitignore

---
+++ integracion_comunitaria/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for integracion_comunitaria project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'integracion_comunitaria.settings')
+
+application = get_wsgi_application()
