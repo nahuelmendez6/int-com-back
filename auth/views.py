d198a6ba8a1f3bc214b6d69b5905bd326ebebd21
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-07-09 00:52:38 -0300

    feat(auth): se implemento RegisterUserAPIView para el registro de usuarios

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+class RegisterUserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'message':'Usuario creado exitosamente',
+                'email': user.email,
+                'role':'provider' if hasattr(user, 'provider') else 'customer'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
