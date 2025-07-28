314a60d8f48da1ba52e7d6ed468834c3640939d0
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-07-28 12:16:15 -0300

    feat(profile): implementacion de ProfileStatusAPIView

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Category, TypeProvider, Profession
 from .serializers import CategorySerializer, TypeProviderSerializer, ProfessionSerializer
 
@@ -13,3 +17,18 @@ class TypeProviderViewSet(viewsets.ModelViewSet):
 class ProfessionViewSet(viewsets.ModelViewSet):
     queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
+
+
+
+class ProfileStatusAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        is_complete = False
+        if hasattr(user, 'provider'):
+            is_complete = user.provider.is_profile_complete()
+
+        return Response({
+            'profile_complete': is_complete
+        })
