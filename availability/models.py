bbd32a97780283814258157889e12402e8f64bca
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-09-29 16:14:58 -0300

    feat(availability): implementacion del modelo Availability

---
+++ availability/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Availability(models.Model):
+
+    """
+    el modelo Availaibility se utiliza para representar la disponibilidad horaria
+    de un proveedor.
+    
+    se define un rango horario start_time - end_time
+
+    cada dia de la semana esta represnetado por enteros del 0 al 6 (0=domingo, 1=lunes, ..., 6=sabado)
+    """
+    
+    id_availability = models.AutoField(primary_key=True)
+    id_provider = models.ForeignKey(
+        'authentication.Provider', on_delete=models.CASCADE, db_column='id_provider', related_name='availabilities'
+    )
+    day_of_week = models.IntegerField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+    class Meta:
+        db_table = 'n_availability'
+        managed = False
