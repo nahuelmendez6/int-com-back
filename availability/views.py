227e42769d61f2f4c604aacf21ce6a58349ebc2c
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-01 19:51:05 -0300

    feat(availability): implementacion de metodo get filtrado por proveedores en  AvailabilityAPIView

--- availability/views.py
+++ availability/views.py
@@ -6,8 +6,17 @@ from rest_framework import status
 from rest_framework.response import Response
 
 class AvaialabilityAPIView(APIView):
+
+    """
+    APIView para crud de disponibilidades
+    """
     
     def post(self, request):
+        
+        """
+        Crea una disponibilidad
+        """
+        
         serializer = AvailabilitySerializer(data=request.data)
 
         if serializer.is_valid():
@@ -18,3 +27,24 @@ class AvaialabilityAPIView(APIView):
             )
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def get(self, request, id_provider):
+        """
+        Devuelve la disponibilidad del provider(id_provider)
+        """
+        availabilitites = Availability.objects.filter(id_provider=id_provider)
+        serializer = AvailabilitySerializer(availabilitites, many=True)
+        
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+
+    def get(self, request, pk=None):
+        """
+        si se pasa pk devuelve una disponibilidad especifica
+        sino devuelve todas
+        """
+        pass
