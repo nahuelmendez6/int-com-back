069a3a6720a28e287ca722d24d032622b05b620e
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-01 19:45:13 -0300

    feat(availability): implementacion de AvailabilityAPIView

--- availability/views.py
+++ availability/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import Availability
+from .serializers import AvailabilitySerializer
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+class AvaialabilityAPIView(APIView):
+    
+    def post(self, request):
+        serializer = AvailabilitySerializer(data=request.data)
+
+        if serializer.is_valid():
+            availability = serializer.save(many=True)
+            return Response(
+                serializer.validated_data, 
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
