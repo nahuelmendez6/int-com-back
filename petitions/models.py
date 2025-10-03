c63a2f287b6606c31d9d49401bfbe03d17731d6d
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-02 21:55:05 -0300

    feat(petitions): Implementacion de modelo Petition

--- petitions/models.py
+++ petitions/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class TypePetitions(models.Model):
+class TypePetition(models.Model):
     
     id_type_petition = models.AutoField(primary_key=True)
     type_petition = models.CharField(max_length=100)
@@ -24,4 +24,37 @@ class PetitionState(models.Model):
     class Meta:
         db_table = 'n_petition_state'
         managed = False
-        
+
+
+class Petition(models.Model):
+
+    id_petition = models.AutoField(primary_key=True)
+    id_type_petition = models.ForeignKey(TypePetition, on_delete=models.PROTECT, db_column='id_type_petition')
+    id_customer = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    id_profession = models.ForeignKey('Profession', on_delete=models.SET_NULL, db_column='id_profession', null=True, blank=True)
+    id_type_provider = models.ForeignKey('TypeProvider', on_delete=models.SET_NULL, db_column='id_type_provider', null=True, blank=True)
+    id_state = models.ForeignKey(PetitionState, on_delete=models.SET_NULL, db_column='id_state', null=True, blank=True)
+    date_since = models.DateField(null=True, blank=True)
+    date_until = models.DateField(null=True, blank=True)
+    id_user_create = models.IntegerField(null=True, blank=True)
+    id_user_update = models.IntegerField(null=True, blank=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    categories = models.ManyToManyField('Category', through='PetitionCategory')
+
+    class Meta:
+        db_table = 'n_petition'
+        managed = False
+
+
+class PetitionCategory(models.Model):
+
+    id_petition_category = models.AutoField(primary_key=True)
+    id_petition = models.ForeignKey(Petition, on_delete=models.CASCADE, db_column='id_petition')
+    id_category = models.ForeignKey('Category', on_delete=models.CASCADE, db_column='id_category')
+
+    class Meta:
+        db_table = 'n_petition_category'
+        managed = False
