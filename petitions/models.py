d73d4ae9d873ca69ce6592b8e8d4a0f79403e65a
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-02 21:52:16 -0300

    feat(petition): Implementacion de modelos TypePetitions y PetitionState

--- petitions/models.py
+++ petitions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class TypePetitions(models.Model):
+    
+    id_type_petition = models.AutoField(primary_key=True)
+    type_petition = models.CharField(max_length=100)
+    id_user_create = models.IntegerField(null=True, blank=True)
+    id_user_update = models.IntegerField(null=True, blank=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'n_type_petition'
+        managed = False
+
+
+class PetitionState(models.Model):
+
+    id_state = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        db_table = 'n_petition_state'
+        managed = False
+        
