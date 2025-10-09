2263f527c34d3d3b22b615250f04058cd09c293e
nahuelmendez6 <mendezmaturano6@gmail.com>
2025-10-09 00:55:11 -0300

    feat(interest): se implemento InterestAPIView con metodos get y post

--- interest/views.py
+++ interest/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from authentication.models import Customer
+from .models import Interest
+from .serializers import InterestSerializer
+
+class InterestAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+
+        user = request.user
+        customer = Customer.objects.filter(id_user=user.id_user).first()
+        id_customer = customer.id_customer
+
+        interest = Interest.objects.filter(id_customer=id_customer, is_deleted=False)
+        serializer = InterestSerializer(interest, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        user = request.user
+        customer = Customer.objects.filter(id_user=user.id_user).first()
+        id_customer = customer.id_customer
+
+        serializer = InterestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            interest = serializer.save(id_customer=id_customer)
+            return Response(InterestSerializer(interest).data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
